47f6246d65e7bc9ffff8291b87dcf9cc34ce1119
Sarahy <sarahym0703@gmail.com>
2023-10-29 15:21:38 -0400

    added more javascript functions to have a timer and to view all questions

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,19 @@
-var quiz = document.querySelector(".site");
+var site = document.querySelector(".site");
+var quiz = document.querySelector(".quiz");
+var final = document.querySelector(".final");
 var questionElement = document.querySelector(".questions");
 var answerButtons = document.querySelector(".answer-buttons");
-var startButton =document.querySelector(".start-button");
+var startButton = document.querySelector(".start-button");
+var timerElement = document.querySelector(".timer");
+var score = document.querySelector(".score");
 
-var score = 0;
+//var score = 0;
 var currentQuestionIndex = 0;
+var highScore;
 var timer;
 var timerCount;
 
-//questions and answers for quiz
+//array of questions and answers for quiz
 var questions = [
     {
         question: "Commonly used data types DO Not Include:",
@@ -38,7 +43,7 @@ var questions = [
         ]
     },
     {
-        question: "string values must be enlocsed within ________ when being assigned to variables.",
+        question: "String values must be enclosed within ________ when being assigned to variables.",
         answers: [
             { text: "1. commas", correct: false},
             { text: "2. curly brackets", correct: true},
@@ -50,7 +55,7 @@ var questions = [
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
         answers: [
             { text: "1. Javascript", correct: false},
-            { text: "2. terminal/bash", correct: false},
+            { text: "2. terminal or bash", correct: false},
             { text: "3. for loops", correct: true},
             { text: "4. console.log", correct: false},
         ]
@@ -59,12 +64,47 @@ var questions = [
 
 
 
-//function to start the quiz
+//function to start the quiz, still working on it
 function startQuiz() {
+    //isWin = false;
     currentQuestionIndex = 0;
     timerCount = 75;
+    score.textContent = timerCount;
+    startTimer();
     showQuestion();
+    site.classList.add("hide");
+    quiz.classList.remove("hide");
 }
+
+
+//function to start timer, and triggers stop, and when game ends if you lose or win
+function startTimer() {
+    timer = setInterval(function() {
+      timerCount--;
+      timerElement.textContent = timerCount;
+      if (timerCount >= 0) {
+        if (currentQuestionIndex >= questions.length && timerCount > 0) {
+          clearInterval(timer);
+          
+
+        }
+        
+        
+        
+      }
+      if (timerCount === 0) {
+        clearInterval(timer);
+        endQuiz();
+      }
+    }, 1000);
+  }
+
+  //function to end quiz if timer reaches 0 before being completed
+  function endQuiz() {
+    quiz.classList.add("hide");
+    final.classList.remove("hide");
+  }
+
 
 //function to display questions and answer choices
 function showQuestion() {
@@ -73,51 +113,97 @@ function showQuestion() {
     var questionNo =currentQuestionIndex + 1;
     questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
-    currentQuestion.answers.forEach(answer => {
+
+    currentQuestion.answers.forEach((answer, index) => {
         var button = document.createElement("button");
         button.innerHTML = answer.text;
         button.classList.add("btn");
         answerButtons.appendChild(button);
-        if(answer.correct) {
-            button.dataset.correct = answer.correct
-        }
+        button.dataset.index = index;
         button.addEventListener("click", selectAnswer);
     });
 }
 
+//to not show original question and answer choice from html file
 function resetState() {
     while(answerButtons.firstChild){
         answerButtons.removeChild(answerButtons.firstChild);
     }
 }
 
+//shows the answer is correct/wrong, maybe same function can be used to throw in the next question, and to add a point to score
 function selectAnswer(e) {
-    var selectedBtn = e.target;
-    var isCorrect = selectedBtn.dataset.correct === "true";
-    if(isCorrect) {
-        score++;
+    var question = questions[currentQuestionIndex];
+    var answerIndex = e.target.dataset.index;
+    if(question.answers[answerIndex].correct) {
+        console.log("correct");
+    } else {
+        console.log("wrong!");
+        timerCount = timerCount-10;
+   }
+
+    currentQuestionIndex++;
+   
+
+    if (currentQuestionIndex >= questions.length) {
+        showFinal();
+    } else {
+        showQuestion();
     }
+    score.textContent = timerCount;
 }
-//isCorrect.addEventListener("click", ()=>{
-    //if(currentQuestionIndex < questions.length) {
-        
-    //}
-//})
-
-startQuiz();
 
 
+// function for final page where scores are there, final score is there, and buttons are also shown
+function showFinal() {
+    quiz.classList.add("hide");
+    final.classList.remove("hide");
 
+
+    //var finalTime = highScore; 
+    var highScoreContainer = document.getElementById('High-score-container');
+    highScoreContainer.innerText = score.textContent;
+
+    var goBackButton = document.querySelector(".go-back"); 
+    goBackButton.addEventListener("click", goBack);
+}
+
+
+//function made for event listener to go back to main page
+function goBack() {
+    final.classList.add("hide");
+    site.classList.remove("hide");
+}
+
+
+//function for scores
+function setScore() {
+    score.textContent = scoreCounter;
+    localStorage.setItem("scoreCount", scoreCounter);
+  }
+
+  // Get stored value from client storage, if it exists
+  function getScore() {
+    var storedScores = localStorage.getItem("winCount");
+    if (storedScores === null) {
+      winCounter = 0;
+    } else {
+      winCounter = storedWins;
+    }
+    win.textContent = winCounter;
+  }
 
 
 startButton.addEventListener("click", startQuiz);
 
 
 
-var goBackButton = document.querySelector(".go-back");
-//create a function to go back, name it goBack
-goBackButton.addEventListener("click", goBack);
 
-var clearScore = document.querySelector(".clear-score");
+
+//var goBackButton = document.querySelector(".go-back");
+//create a function to go back, name it goBack
+//goBackButton.addEventListener("click", goBack);
+
+//var clearScore = document.querySelector(".clear-score");
 //create a function to clear high scores, name it clearHighScore
-clearScore = addEventListener("click", clearHighScore);
+//clearScore = addEventListener("click", clearHighScore);
